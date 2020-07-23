0ad6d736f22c540d3c205b315d8cf75174eabd2d
Tyler Alsop <tylerjalsop@gmail.com>
2020-07-22 19:13:09 -0600

    Imported and then commented out the components to use later.

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+// import Grid from './components/grid/grid'
+// import Buttons from './components/buttons/buttons'
+// import Rules from './components/rules/rules'
 
 function App() {
   return (
@@ -7,6 +10,10 @@ function App() {
       <header className="App-header">
         <h1 className="App-title">Conway's Game of Life</h1>
         <h3>Built by Tyler Alsop</h3>
+        <div className="game-container">
+          {/* <Grid className="grid"></Grid>
+          <Buttons className="buttons"></Buttons> */}
+        </div>
 
       </header>
     </div>
