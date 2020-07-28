6eb7c36830950bf270b984614bd1471594917a44
Tyler Alsop <tylerjalsop@gmail.com>
2020-07-27 19:43:47 -0600

    Created the array needed for the grid, the css for the grid, and installed immer to help with the state changes.

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -12,8 +12,24 @@ import Grid from './components/grid/grid'
 // import Buttons from './components/buttons/buttons'
 import Rules from './components/rules/rules'
 
+
+
+const rowsNumber = 50;
+const columnsNumber = 50;
+
+
 function App() {
-  const [grid, setGrid] = useState([]);
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < rowsNumber; i++) {
+      rows.push(Array.from(Array(columnsNumber), () => 0))
+    }
+    return rows
+  });
+
+  console.log(grid)
+
+
   const enlargeGrid = dimensions => {
     console.log("Grid dimensions in enlargeGrid()", dimensions)
     setGrid([...grid, dimensions])
@@ -27,11 +43,38 @@ function App() {
           <h3>Built by Tyler Alsop</h3>
         </header>
         <div className="game-container">
-            <Grid className="grid"></Grid>
-            {/* <Buttons className="buttons"></Buttons> */}
             <Route path="/rules">
               <Rules />
             </Route>
+            <div 
+              className="temp-grid-container"
+              style={{
+                display: 'grid',
+                gridTemplateColumns: `repeat(${columnsNumber}, 20px)`
+              }}
+            >
+              {grid.map((rows, i) => 
+                rows.map((column, j) => (
+                  <div 
+                    className="actual-grid"
+                    key = {`${i}-${j}`}
+                    onClick = {() => {
+                      grid[i][j]
+                    }}
+                    style={{
+                      width: 20, 
+                      height: 20, 
+                      backgroundcolor: grid[i][j] ? "black" : "lightgray", 
+                      border: "solid 1px black"
+                    }}
+                  />
+                ))
+              )}
+
+            </div>
+            <Grid className="grid"></Grid>
+            {/* <Buttons className="buttons"></Buttons> */}
+
           </div>
       </div>
     </GridContext.Provider>
