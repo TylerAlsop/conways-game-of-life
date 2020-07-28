36c2a5224a241a6fe8b961ebda1b5dc8c828f47e
Tyler Alsop <tylerjalsop@gmail.com>
2020-07-27 19:01:43 -0600

    Updated some of the styling. Worked on the grid.js.

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -8,28 +8,31 @@ import './App.css';
 import GridContext from './contexts/GridContext'
 
 ///////// Components /////////
-// import Grid from './components/grid/grid'
+import Grid from './components/grid/grid'
 // import Buttons from './components/buttons/buttons'
 import Rules from './components/rules/rules'
 
 function App() {
   const [grid, setGrid] = useState([]);
+  const enlargeGrid = dimensions => {
+    console.log("Grid dimensions in enlargeGrid()", dimensions)
+    setGrid([...grid, dimensions])
+  }
 
   return (
-    <GridContext.Provider value={{grid}}>
+    <GridContext.Provider value={{grid, enlargeGrid}}>
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Conway's Game of Life</h1>
           <h3>Built by Tyler Alsop</h3>
-          <div className="game-container">
-            {/* <Grid className="grid"></Grid>
-            <Buttons className="buttons"></Buttons> */}
+        </header>
+        <div className="game-container">
+            <Grid className="grid"></Grid>
+            {/* <Buttons className="buttons"></Buttons> */}
             <Route path="/rules">
               <Rules />
             </Route>
           </div>
-
-        </header>
       </div>
     </GridContext.Provider>
   );
